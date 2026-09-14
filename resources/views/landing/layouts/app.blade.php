d91149ccffe17634291874d9e98716598b610466
mochammadaldiansyah <gakurealdi@gmail.com>
2026-09-14 08:37:44 +0700

    fix: restructure folder structure

--- resources/views/landing/layouts/app.blade.php
+++ resources/views/landing/layouts/app.blade.php
@@ -21,7 +21,8 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+             @yield('content')
+             
             </main>
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [LoginController::class, 'login'])->name('login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
